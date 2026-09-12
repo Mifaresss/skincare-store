176b6e49ea84e78976bbbb35f0ae51bb9faecdae
Mifaresss <9803211@gmail.com>
2026-09-12 23:07:09 +0300

    chore: add biome, prettier and lefthook

--- cms/config/database.ts
+++ cms/config/database.ts
@@ -1,13 +1,13 @@
-import path from 'path';
-import type { Core } from '@strapi/strapi';
+import path from 'node:path';
 import { isDatabaseClientKind } from '@strapi/database';
+import type { Core } from '@strapi/strapi';
 
 const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Database => {
   const client = env('DATABASE_CLIENT', 'sqlite');
 
   if (!isDatabaseClientKind(client)) {
     throw new Error(
-      `Unsupported DATABASE_CLIENT: ${client}. Use "postgres", "mysql", or "sqlite".`
+      `Unsupported DATABASE_CLIENT: ${client}. Use "postgres", "mysql", or "sqlite".`,
     );
   }
 
